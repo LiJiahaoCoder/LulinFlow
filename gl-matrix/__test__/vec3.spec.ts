a59c35ff2bb3fa1a35ac0755e535e558030f2363
Jiahao Li <lijiahao_shandong@163.com>
2020-06-17 09:07:52 +0800

    [Test] Surround test cases with describe

--- gl-matrix/__test__/vec3.spec.ts
+++ gl-matrix/__test__/vec3.spec.ts
@@ -1,26 +1,28 @@
 import Vec3 from '../src/vec3';
 
-it('should get initial vec3', () => {
-  const vec3 = new Vec3();
+describe('Tests of vec3', () => {
+  it('should get initial vec3', () => {
+    const vec3 = new Vec3();
 
-  expect( vec3.x ).toBe( 0.0 );
-  expect( vec3.y ).toBe( 0.0 );
-  expect( vec3.z ).toBe( 0.0 );
-});
+    expect( vec3.x ).toBe( 0.0 );
+    expect( vec3.y ).toBe( 0.0 );
+    expect( vec3.z ).toBe( 0.0 );
+  });
 
-it('should get initial vec3 with specified value', () => {
-  const vec3 = new Vec3({ x: 1.0, y: 2.0, z: 3.0 });
+  it('should get initial vec3 with specified value', () => {
+    const vec3 = new Vec3({ x: 1.0, y: 2.0, z: 3.0 });
 
-  expect( vec3.x ).toBe( 1.0 );
-  expect( vec3.y ).toBe( 2.0 );
-  expect( vec3.z ).toBe( 3.0 );
-});
+    expect( vec3.x ).toBe( 1.0 );
+    expect( vec3.y ).toBe( 2.0 );
+    expect( vec3.z ).toBe( 3.0 );
+  });
 
-it('should get the same vec3 when call clone static method', () => {
-  const source = new Vec3({ x: 1.0, y: 1.0, z: 1.5 });
-  const cloned = Vec3.clone( source );
+  it('should get the same vec3 when call clone static method', () => {
+    const source = new Vec3({ x: 1.0, y: 1.0, z: 1.5 });
+    const cloned = Vec3.clone( source );
 
-  expect( source.x ).toBe( cloned.x );
-  expect( source.y ).toBe( cloned.y );
-  expect( source.z ).toBe( cloned.z );
+    expect( source.x ).toBe( cloned.x );
+    expect( source.y ).toBe( cloned.y );
+    expect( source.z ).toBe( cloned.z );
+  });
 });
