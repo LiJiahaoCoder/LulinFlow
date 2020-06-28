858d34c4ad7a4b61cdd2fd456f79e4aef9b9d862
Jiahao Li <lijiahao_shandong@163.com>
2020-06-28 22:45:16 +0800

    [Refactor] Rename delta to t

--- gl-matrix/src/vec2.ts
+++ gl-matrix/src/vec2.ts
@@ -213,16 +213,16 @@ export default class Vec2 {
    *
    * @param {Vec2} v1 First vec2
    * @param {Vec2} v2 Second vec2
-   * @param {number} delta Interpolation amount, in the range [0-1]
+   * @param {number} t Interpolation amount, in the range [0-1]
    * @returns {Vec2} Vec2
    */
-  static lerp ( v1: Vec2, v2: Vec2, delta: number): Vec2 {
+  static lerp ( v1: Vec2, v2: Vec2, t: number): Vec2 {
     return new Vec2({
       x: numeral( v1.x ).add(
-        delta * numeral( v2.x ).subtract( v1.x ).value(),
+        t * numeral( v2.x ).subtract( v1.x ).value(),
       ).value(),
       y: numeral( v1.y ).add(
-        delta * numeral( v2.y ).subtract( v1.y ).value(),
+        t * numeral( v2.y ).subtract( v1.y ).value(),
       ).value(),
     });
   }
